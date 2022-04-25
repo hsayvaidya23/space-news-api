3b760fe3f44970aafbf631cd686f7162579ea758
Yash Vaidya <bobbynaldocr7@gmail.com>
2022-04-25 18:30:03 +0530

    Fetching data from the API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import "./App.css";
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    fetch("https://newsapi.org/v2/everything?q=space&apiKey=857c2f8193ac4125aa7ae4f9129e1ccc")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+  })
+  
+
   return (
     <div className="App">
       <div className="title">
